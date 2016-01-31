8a7e53b69e3f3e7bf50091b3c0e030293e9057da
m2fan <2fani.clash@gmail.com>
2016-01-31 22:06:31 +0330

    Update inv.lua

--- plugins/inv.lua
+++ plugins/inv.lua
@@ -1,20 +1,20 @@
 do
-local function callbackres(extra, success, result) 
+local function callbackres(extra, success, result) -- Callback for res_user in line 27
   local user = 'user#id'..result.id
-  local chat = 'chat#id'..extra.chatid
-  if is_banned(result.id, extra.chatid) then 
+	local chat = 'chat#id'..extra.chatid
+	if is_banned(result.id, extra.chatid) then -- Ignore bans
             send_large_msg(chat, 'User is banned.')
-  elseif is_gbanned(result.id) then 
-      send_large_msg(chat, 'User is globaly banned.')
-  else    
-      chat_add_user(chat, user, ok_cb, false) 
-  end
+	elseif is_gbanned(result.id) then -- Ignore globall bans
+	    send_large_msg(chat, 'User is globaly banned.')
+	else    
+	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
+	end
 end
 function run(msg, matches)
   local data = load_data(_config.moderation.data)
   if not is_realm(msg) then
     if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin(msg) then
-      return 'Group is private.'
+		  return 'Group is private.'
     end
   end
   if msg.to.type ~= 'chat' then 
@@ -23,18 +23,17 @@ function run(msg, matches)
   if not is_momod(msg) then
     return
   end
-  if not is_admin(msg) then 
-    return 'Only admins can invite.'
-  end
-  local cbres_extra = {chatid = msg.to.id}
+  --if not is_admin(msg) then -- For admins only !
+    --return 'Only admins can invite.'
+  --end
+	local cbres_extra = {chatid = msg.to.id}
   local username = matches[1]
   local username = username:gsub("@","")
   res_user(username,  callbackres, cbres_extra)
 end
 return {
     patterns = {
-      "^[!/$&#@]invite (.*)$",
-      "^([Ii]nvite (.*)$"
+      "^[@#$%^&*!/]invite (.*)$"
     },
     run = run
 }
