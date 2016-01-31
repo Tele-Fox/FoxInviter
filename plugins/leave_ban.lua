2010034a25f8048e6f691f89b5ed83712f83f770
m2fan <2fani.clash@gmail.com>
2016-01-31 15:30:05 +0330

    Update leave_ban.lua

--- plugins/leave_ban.lua
+++ plugins/leave_ban.lua
@@ -1,26 +1,22 @@
-local function run(msg, matches)
-  local data = load_data(_config.moderation.data)
-   if msg.action and msg.action.type then
-   local action = msg.action.type 
-    if data[tostring(msg.to.id)] then
-      if data[tostring(msg.to.id)]['settings'] then
-        if data[tostring(msg.to.id)]['settings']['leave_ban'] then 
-          leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
-        end
+if msg.action and msg.action.type then
+    local action = msg.action.type
+    if action == 'chat_del_user' then
+      local user_id = msg.from.id
+    if data[tostring(msg.to.id)]['settings']['lock_leave'] == 'yes' then
+  if not is_momod(msg) then
+  chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+    local msg = 'Join link is Locked !'
+   local receiver = msg.to.id
+    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+  
       end
-    end
-   if action == 'chat_del_user' and not is_momod2(msg.action.user.id) and leave_ban == 'yes' then
-     	local user_id = msg.action.user.id
-     	local chat_id = msg.to.id
-     	ban_user(user_id, chat_id)
-     end
    end
-  end
-
-
+end
 return {
-  patterns = {
-    "^!!tgservice (.*)$"
-  },
-  run = run
+   description = "to ban the leaver",
+   usage = "!lock leave : no one can come back to group who left once",
+   patterns = {
+      "^!!tgservice (chat_del_user)$"
+},
+   run = run
 }
